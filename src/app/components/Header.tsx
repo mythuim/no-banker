1415bf922b7e92eb34995d752cbf67370e1575db
Mythuim <mythuim@proton.me>
2025-08-15 14:48:19 +0200

    Update Header layout and add Mythuim link
    Refactored the Header component to improve layout and styling. Added a centered link to Mythuim's WiseOldMan profile and removed the logo image from the navigation.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,28 +1,30 @@
 const Header = () => {
   return (
-    <header className="">
+    <header className="border-b border-white/5 mb-2">
       <nav
         aria-label="Global"
-        className="mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8 pt-4 pb-2"
+        className="mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8 py-4"
       >
-        <div className="flex">
-          <a
-            href="/"
-            className="-m-1.5 p-1.5 flex items-center text-white/50 hover:text-white transition-colors font-bold"
-          >
-            NoBanker
-            <img alt="NoBanker" src="/uim.ico" className="h-8 w-auto" />
-          </a>
-        </div>
-        <div className="flex flex-1 justify-end">
-          <a
-            href="https://github.com/mythuim/no-banker"
-            target="_blank"
-            className="text-xs font-semibold text-white/50 hover:text-white transition-colors"
-          >
-            GitHub <span aria-hidden="true">&rarr;</span>
-          </a>
-        </div>
+        <a
+          href="/"
+          className="-m-1.5 p-1.5 flex items-center text-white/50 hover:text-white transition-colors font-bold"
+        >
+          NoBanker
+        </a>
+        <a
+          href="https://wiseoldman.net/players/Mythuim"
+          target="_blank"
+          className="font-bold text-xs text-kharid/75 hover:text-kharid transition-colors flex flex-1 justify-center"
+        >
+          Mythuim
+        </a>
+        <a
+          href="https://github.com/mythuim/no-banker"
+          target="_blank"
+          className="text-xs font-semibold text-white/50 hover:text-white transition-colors"
+        >
+          GitHub <span aria-hidden="true">&rarr;</span>
+        </a>
       </nav>
     </header>
   );
